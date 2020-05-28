593003ade1ec0abcb70e920a81ff825d6cca4002
Ashkan <ashkanbeeu@gmail.com>
2020-05-28 15:32:31 +0430

    add and adjastment tempaltes folder

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1> this is from view</h1>')
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
