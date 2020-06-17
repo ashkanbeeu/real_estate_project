3bdb7d6aa0ae468b8b7446b1da992ab28968ae4a
Ashkan <ashkanbeeu@gmail.com>
2020-06-17 19:31:43 +0430

    create dynamic Listings

--- listings/urls.py
+++ listings/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-  path('', views.index, name = 'listings_index'),
-  path('<int:listing_id>', views.listing, name = 'listings_listing'),
-  path('search/', views.search, name = 'listings_search'),
+  path('', views.index, name='listings_index'),
+  path('<int:listing_id>', views.listing, name='listings_listing'),
+  path('search/', views.search, name='listings_search'),
 ]
