69fef4134382dfd0e6c0cb4693d82dad0c96e3ae
Ashkan <ashkanbeeu@gmail.com>
2020-06-17 16:05:14 +0430

    customize admin panel

--- listings/admin.py
+++ listings/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Listing
 
+class ListingsAdmin(admin.ModelAdmin):
+  list_display = ( 'id', 'title', 'is_published', 'price', 'list_date', 'realtor' )
+  list_display_links = ( 'id', 'title' )
+  list_filter = ('realtor',)
+  search_fields = ('title', 'description')
+  list_per_page = 20
 
-admin.site.register(Listing)
+
+admin.site.register(Listing, ListingsAdmin)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Realtor
 
+class RealtorAdmin(admin.ModelAdmin):
+  list_display = ( 'name', 'phone', 'email', 'is_mvp' )
 
-admin.site.register(Realtor)
+admin.site.register(Realtor, RealtorAdmin)
